7ebf746746947ba0159915c59ed27e1442478cca
ankursharma2725 <fantastic4talks@gmail.com>
2023-04-12 17:05:05 +0530

    shivu1

---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+repoRoot=$(realpath "$(dirname "$0")")
+
+cd "$repoRoot"
+
+## sh "./run.sh '${component}'" 
+
+
+COMPONENT="$1"
+if [[ -z "$COMPONENT" ]]; then
+    echo "COMPONENT is not set"
+    exit 1
+fi
+
+
+
+case "$COMPONENT" in
+elastic)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-elastic.yml --connection=local
+    ;;
+kibana)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-kibana.yml --connection=local
+    ;;
+configurator)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-configurator.yml --connection=local 
+    ;;
+heartbeat)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-heartbeat.yml --connection=local 
+    ;;
+metricbeat)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-metricbeat.yml --connection=local 
+    ;;
+filebeat)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-filebeat.yml --connection=local 
+    ;;
+auditbeat)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-auditbeat.yml --connection=local 
+    ;;                         
+kafka)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-kafka.yml --connection=local 
+    ;;
+kafka-ui)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-kafka-ui.yml --connection=local 
+    ;;    
+promethues)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-promethues.yml --connection=local 
+    ;;
+grafana)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-grafana.yml --connection=local 
+    ;;
+jenkins)
+    ansible-playbook -i environments/localhost/ --vault-password-file=~/.secrets/tass-ansible-vault deploy-jenkins.yml --connection=local 
+    ;;             
+*) ;;
+esac
